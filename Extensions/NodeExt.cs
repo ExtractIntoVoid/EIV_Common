526083b03c08075228d8453933e966555235a119
SlejmUr <slejmur@protonmail.com>
2024-08-28 21:34:46 +0200

    adding simple nodeExt

---
+++ Extensions/NodeExt.cs
@@ -0,0 +1,15 @@
+﻿using Godot;
+
+namespace EIV_Common.Extensions;
+
+public static class NodeExt
+{
+    public static void SetMP(this Node node, int NetId)
+    {
+        if (node == null)
+            return;
+        node.SetMultiplayerAuthority(NetId);
+        if (node.HasNode("MSync"))
+            node.GetNode<MultiplayerSynchronizer>("MSync").SetMultiplayerAuthority(NetId);
+    }
+}
